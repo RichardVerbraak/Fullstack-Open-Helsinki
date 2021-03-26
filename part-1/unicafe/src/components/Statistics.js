aa00c1fee3080da023551311fefb6c2e9d816d58
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-03-26 20:00:59 +0100

    Changed statistics to HTML table

--- part-1/unicafe/src/components/Statistics.js
+++ part-1/unicafe/src/components/Statistics.js
@@ -7,14 +7,39 @@ const Statistics = ({ good, neutral, bad, all }) => {
 			{!all ? (
 				<p>No feedback given</p>
 			) : (
-				<div>
-					<p>good {good}</p>
-					<p>neutral {neutral}</p>
-					<p>bad {bad}</p>
-					<p>all {all}</p>
-					<p>average {(good + neutral * 0 + bad * -1) / all}</p>
-					<p>positive {(good / all) * 100} %</p>
-				</div>
+				<table>
+					<tbody>
+						<tr>
+							<td>good </td>
+							<td>{good}</td>
+						</tr>
+
+						<tr>
+							<td>neutral </td>
+							<td>{neutral}</td>
+						</tr>
+
+						<tr>
+							<td>bad </td>
+							<td>{bad}</td>
+						</tr>
+
+						<tr>
+							<td>all </td>
+							<td>{all}</td>
+						</tr>
+
+						<tr>
+							<td>average </td>
+							<td>{(good + neutral * 0 + bad * -1) / all}</td>
+						</tr>
+
+						<tr>
+							<td>positive </td>
+							<td>{(good / all) * 100} %</td>
+						</tr>
+					</tbody>
+				</table>
 			)}
 		</div>
 	)
