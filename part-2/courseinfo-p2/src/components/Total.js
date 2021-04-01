4b4b067ea3898ee8f0f097f95726c74a931d1d6d
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-04-01 16:21:06 +0200

    Added part-2 course info partially completed

---
+++ part-2/courseinfo-p2/src/components/Total.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Total = ({ parts }) => {
+	return (
+		<div>
+			<strong>
+				total of{' '}
+				{parts.reduce((sum, part) => {
+					return sum + part.exercises
+				}, 0)}{' '}
+				exercises
+			</strong>
+		</div>
+	)
+}
+
+export default Total
